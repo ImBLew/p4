61e75e446445b34c2637efd7121a839acc217e3b
Anthony <aleung4@wisc.edu>
2018-04-12 02:23:12 -0500

    Finish WordProcessor.java
    Implemented getWordStream() and isAdjacent()

--- src/WordProcessor.java
+++ src/WordProcessor.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
+import java.util.*;
 
 /**
  * This class contains some utility helper methods
@@ -64,8 +66,10 @@ public class WordProcessor {
 		 * Note: since map and filter return the updated Stream objects, they can chained together as:
 		 * 		streamOfLines.map(...).filter(a -> ...).map(...) and so on
 		 */
-		
-		return null;
+	    
+	    Stream<String> stream = Files.lines(Paths.get(filepath)).map(String::trim).map(String::toUpperCase);
+
+	    return stream;
 	}
 	
 	/**
@@ -86,7 +90,86 @@ public class WordProcessor {
 	 * @return true if word1 and word2 are adjacent else false
 	 */
 	public static boolean isAdjacent(String word1, String word2) {
-		return false;	
+        int numDiffChar = 0;
+
+	    if (word1.equals(word2)) {
+	        return false;
+	    }
+	    
+	    int lengthDiff = Math.abs(word1.length() - word2.length());
+	    if (lengthDiff > 1) {
+	        return false; 
+	    }
+	    // Check replacement 
+	    if (word1.length() == word2.length()) {
+	        int wordLength = word1.length();
+	        
+	        for (int i = 0, j = wordLength -1, counter = 0; counter <= wordLength/2; i++, j--, counter++) {
+	            char left1 = word1.charAt(i);
+	            char right1 = word1.charAt(j);
+	            char left2 = word2.charAt(i);
+	            char right2 = word2.charAt(j);
+	            
+	            if (left1 != left2) {
+	                numDiffChar++;
+	            }	   
+	            
+	            // Avoid checking on the middle char if the length of words are odd
+	            if (i != j) {
+	                if (right1 != right2) {
+	                    numDiffChar++;
+	                }
+	            }
+  
+	            if (numDiffChar > 1) {
+	                return false;
+	            }
+	        }
+	    }
+	    
+	    if (word1.length() != word2.length()) {
+    	    // Check addition and deletion
+    	    int diffCharIndex = 0;
+    	    String longerWord = word1.length() > word2.length() ? word1 : word2;
+    	    String shorterWord = word1.length() < word2.length() ? word1 : word2;
+    	    
+    	    // Look for the index where the two words are starting to differ
+    	    for (int i = 0; i < longerWord.length(); i++) {
+    	        if (word1.charAt(i) != word2.charAt(i)) {
+    	            diffCharIndex = i;
+    	            break;
+    	        }
+    	    }
+    	    
+    	    String beforeDiffChar1 = longerWord.substring(0, diffCharIndex);
+    	    String beforeDiffChar2 = shorterWord.substring(0, diffCharIndex);
+    	    String afterDiffChar1 = longerWord.substring(diffCharIndex + 1, longerWord.length());
+    	    String afterDiffChar2 = shorterWord.substring(diffCharIndex, shorterWord.length());
+    	    
+    	    if (!(beforeDiffChar1.equals(beforeDiffChar2)) || !(afterDiffChar1.equals(afterDiffChar2))) {
+    	        return false;
+    	    }
+	    }
+    
+		return true;	
+	}
+	
+	public static void main(String[] args) {
+//	    try {
+//	        Stream<String> stream = getWordStream("word_list.txt");
+//	        Object[] string = stream.toArray();
+//	        for (Object o : string)
+//	            System.out.println(o);
+//	    } catch (Exception e ) { 
+//	        e.printStackTrace();
+//	    }
+
+	    
+	    System.out.println(isAdjacent("meet","met"));
+	    System.out.println(isAdjacent("met","met"));
+	    System.out.println(isAdjacent("heiah","heaih"));
+	    System.out.println(isAdjacent("were","where"));
+	    
 	}
 	
 }
