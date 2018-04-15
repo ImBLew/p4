7f8365de31074428c32342c05b77277c1808ed2a
yaakovlevin78 <36935038+yaakovlevin78@users.noreply.github.com>
2018-04-15 18:10:31 -0500

    Update GraphProcessor.java

--- src/GraphProcessor.java
+++ src/GraphProcessor.java
@@ -1,3 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////
+//                   
+// Class File:  	 Graph.java
+// Semester:         Spring 2018
+//
+// Author:           Yaakov Levin, Anthony Leung, Haoran Li, Ben Lewis
+// Credits:          none
+/////////////////////////////////////////////////////////////////////////////////
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
