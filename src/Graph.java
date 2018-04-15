21fca4019cad17692301ab16e293460e440de3c7
yaakovlevin78 <36935038+yaakovlevin78@users.noreply.github.com>
2018-04-15 14:39:59 -0500

    Update Graph.java

--- src/Graph.java
+++ src/Graph.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Stream;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 /**
  * Undirected and unweighted graph implementation
@@ -15,13 +19,40 @@ public class Graph<E> implements GraphADT<E> {
     /**
      * Instance variables and constructors
      */
-
+	private ArrayList<E> Vertices;
+	private ArrayList<ArrayList<Integer>> Matrix;
+	
+	public Graph() {
+		Vertices = new ArrayList<E>();
+		Matrix = new ArrayList<ArrayList<Integer>>();
+	}
+	
     /**
      * {@inheritDoc}
      */
     @Override
     public E addVertex(E vertex) {
+        if (vertex == null)
+        	return null;
         
+        for(int i =0; i< Vertices.size(); i++) {
+        	if(Vertices.get(i).equals(vertex))
+        		return null;
+        }
+        
+        Vertices.add(vertex);
+        Matrix.add(new ArrayList<Integer>(Vertices.size()));
+        //size()-1 because don't need to add edges for itself
+        for(int i = 0; i< Vertices.size()-1;i++) {
+        	if(WordProcessor.isAdjacent(vertex.toString(), Vertices.get(i).toString())){
+        		Matrix.get(Vertices.size()).set(i,1);
+        	}
+        	else {
+        		Matrix.get(Vertices.size()).set(i,0);
+        	}
+        }
+        
+        return vertex;
     }
 
     /**
@@ -29,7 +60,22 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public E removeVertex(E vertex) {
+        if(vertex == null)		
+        	return null;
         
+        //likely to have bugs
+        for(int i = 0;i<Vertices.size(); i++) {
+        	//.equals could be causing a problem
+        	if(Vertices.get(i).equals(vertex)) {
+        		Matrix.remove(i);
+        		for(int j=0; j < Matrix.size(); j++) {
+        			Matrix.get(j).remove(i+1);
+        		}
+        		return vertex;
+        	}
+        }
+        
+        return null;
     }
 
     /**
@@ -37,7 +83,34 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public boolean addEdge(E vertex1, E vertex2) {
-        
+    	if(vertex1.equals(vertex2))
+    		return false;
+    	
+    	int count = 0;
+    	int xCor = 0;
+    	int yCor = 0;
+    	//checks if first vertex is in graph
+    	for(int i =0;i<Vertices.size();i++) {
+    		//.equals could be causing a problem
+    		if(Vertices.get(i).equals(vertex1)) {
+    			count++;
+    			xCor = i;
+    		}
+    	}
+    	//checks if second vertex is in graph
+    	for(int i =0;i<Vertices.size();i++) {
+    		//.equals could be causing a problem
+    		if(Vertices.get(i).equals(vertex2)) {
+    			count++;
+    			yCor = i;
+    		}
+    	}
+  
+    	if(count == 2) {
+    		Matrix.get(xCor).set(yCor,1);    			
+    		return true;
+    	}
+        return false;
     }    
 
     /**
@@ -45,7 +118,34 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public boolean removeEdge(E vertex1, E vertex2) {
-        
+    	if(vertex1.equals(vertex2))
+    		return false;
+    	
+    	int count = 0;
+    	int xCor = 0;
+    	int yCor = 0;
+    	//checks if first vertex is in graph
+    	for(int i =0;i<Vertices.size();i++) {
+    		//.equals could be causing a problem
+    		if(Vertices.get(i).equals(vertex1)) {
+    			count++;
+    			xCor = i;
+    		}
+    	}
+    	//checks if second vertex is in graph
+    	for(int i =0;i<Vertices.size();i++) {
+    		//.equals could be causing a problem
+    		if(Vertices.get(i).equals(vertex2)) {
+    			count++;
+    			yCor = i;
+    		}
+    	}
+  
+    	if(count == 2) {
+    		Matrix.get(xCor).set(yCor,0);    			
+    		return true;
+    	}
+        return false;
     }
 
     /**
@@ -53,7 +153,34 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public boolean isAdjacent(E vertex1, E vertex2) {
-        
+    	if(vertex1.equals(vertex2))
+    		return false;
+    	
+    	int count = 0;
+    	int xCor = 0;
+    	int yCor = 0;
+    	//checks if first vertex is in graph
+    	for(int i =0;i<Vertices.size();i++) {
+    		//.equals could be causing a problem
+    		if(Vertices.get(i).equals(vertex1)) {
+    			count++;
+    			xCor = i;
+    		}
+    	}
+    	//checks if second vertex is in graph
+    	for(int i =0;i<Vertices.size();i++) {
+    		//.equals could be causing a problem
+    		if(Vertices.get(i).equals(vertex2)) {
+    			count++;
+    			yCor = i;
+    		}
+    	}
+  
+    	if(count == 2) {
+    		if(Matrix.get(xCor).get(yCor) == 1)    			
+    		return true;
+    	}
+        return false;  	
     }
 
     /**
@@ -61,7 +188,20 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public Iterable<E> getNeighbors(E vertex) {
-        
+    	ArrayList<E> neighbors = new ArrayList<E>();
+    	int index = 0;
+    	for(int i =0; i<Vertices.size(); i++) {
+    		if(Vertices.get(i).equals(vertex))
+    			index = i;
+    	}
+    	
+    	for(int i=0; i<Matrix.get(index).size();i++) {
+    		if(Matrix.get(index).get(i)==1) {
+    			neighbors.add(Vertices.get(i));
+    		}
+    	}
+    	
+    	return neighbors;
     }
 
     /**
@@ -69,7 +209,7 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public Iterable<E> getAllVertices() {
-        
+    	return Vertices;		   
     }
 
 }
